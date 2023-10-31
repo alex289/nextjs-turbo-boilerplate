7d8b0a050b0ef5ada4994781e0c3e8b5e477e3c5
alex289 <mccr4fthd@gmail.com>
2023-10-31 10:12:12 +0100

    fix: comment out api route

--- apps/web/src/app/api/examples/route.ts
+++ apps/web/src/app/api/examples/route.ts
@@ -1,12 +1,13 @@
-import { prisma } from 'db';
+// import { prisma } from 'db';
 
-export async function GET() {
-  const posts = await prisma.post.findMany();
-  return new Response(JSON.stringify(posts), {
-    status: 200,
-    headers: {
-      'content-type': 'application/json',
-      'cache-control': 'public, s-maxage=60, stale-while-revalidate=30',
-    },
-  });
+export function GET() {
+  // const posts = await prisma.post.findMany();
+  // return new Response(JSON.stringify(posts), {
+  //   status: 200,
+  //   headers: {
+  //     'content-type': 'application/json',
+  //     'cache-control': 'public, s-maxage=60, stale-while-revalidate=30',
+  //   },
+  // });
+  return new Response('Success');
 }
