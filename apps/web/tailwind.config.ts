ed999b6c86180e15474793c47d04236ab69bacc0
alex289 <mccr4fthd@gmail.com>
2023-10-31 10:16:31 +0100

    fix: tailwind content

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,11 +1,7 @@
 import { type Config } from 'tailwindcss';
 
 const config = {
-  content: [
-    './pages/**/*.tsx',
-    './components/**/*.tsx',
-    '../../packages/ui/**/*.tsx',
-  ],
+  content: ['./src/**/*.tsx', '../../packages/ui/**/*.tsx'],
 } satisfies Config;
 
 export default config;
